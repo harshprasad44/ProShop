6d96c652d9526099ffdd95248eb1a9afc13d52f2
Harsh Prasad <explorerharsh@gmail.com>
2020-12-10 19:13:24 +0530

    adding colors to the console/terminal

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     });
 
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`ERROR: ${error.message}`);
+    console.error(`ERROR: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
