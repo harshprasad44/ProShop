f9fdef7ddd709b3f2138d771f32a7dd93d1f2ea6
Harsh Prasad <explorerharsh@gmail.com>
2020-12-20 18:21:07 +0530

    Custom error Handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productsRoutes.js";
@@ -16,6 +17,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
