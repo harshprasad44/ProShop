547e6ac51626dd5aaa265d4c136672fb7b01c96a
Harsh Prasad <explorerharsh@gmail.com>
2021-01-09 02:05:14 +0530

    Admin User Delete

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,6 +9,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
   userListReducer,
+  userDeleteReducer,
 } from "./reducers/userReducers";
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from "./reducers/orderReducers";
 
@@ -21,6 +22,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
