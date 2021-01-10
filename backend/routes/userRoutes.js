f68ecbce8f2f32ad2fbe56e5f855c042750358a5
Harsh Prasad <explorerharsh@gmail.com>
2021-01-11 01:19:27 +0530

    Get user by ID and Update user by id (admins only) endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,12 +7,18 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route("/:id").delete(protect, admin, deleteUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
