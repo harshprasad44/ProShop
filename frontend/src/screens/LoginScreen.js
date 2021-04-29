9fea4939d732840484c4aa6d4dd0876495f2a9ca
Steve Varghese <sv3629@srmist.edu.in>
2021-04-29 16:21:35 +0530

    Sign In Page by Steve

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -62,7 +62,7 @@ const LoginScreen = ({ location, history }) => {
 
       <Row className="py-3">
         <Col>
-          New Customer? <Link to={redirect ? `register/redirect=${redirect}` : "/register"}>Register</Link>
+          New Customer? <Link to={redirect ? `register/redirect=${redirect}` : "/register"}>Sign In</Link>
         </Col>
       </Row>
     </FormContainer>
